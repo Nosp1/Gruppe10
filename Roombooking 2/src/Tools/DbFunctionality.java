de1f95e9687c91b3a2c5caa9ecbc7ec5b0750225
Nosp <trymstaur@hotmail.com>
2019-09-13 14:46:58 +0200

    Fixed more product structure. Added packages
    Added packages

--- Roombooking 2/src/Tools/DbFunctionality.java
+++ Roombooking 2/src/Tools/DbFunctionality.java
@@ -1,3 +1,5 @@
+package Tools;
+
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
--- Roombooking 2/src/Tools/DbTool.java
+++ Roombooking 2/src/Tools/DbTool.java
@@ -1,3 +1,5 @@
+package Tools;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
