e183c447de695a22df9c43c59d0276153ccf787d
Bjørnar W. Risdalen <bwendelborgrisdalen@gmail.com>
2019-09-23 22:03:57 +0200

    Fixed testing. Added tests for Adding, Checking and Deleting a user. Added tests for Adding and Deleting rooms.

--- Roombooking 2/src/Tools/DbTool.java
+++ Roombooking 2/src/Tools/DbTool.java
@@ -5,10 +5,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * @author trym
@@ -28,6 +25,18 @@ public class DbTool {
             e.printStackTrace();
             out.print("naming error" + e);
         }
+        return connection;
+    }
+
+    public Connection dbLogIn() {
+        // dbLogIn without PrintWriter, used for testing
+        connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Roombooking?autoReconnect=true&useSSL=false", "root", "toor");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return connection;
     }
 
