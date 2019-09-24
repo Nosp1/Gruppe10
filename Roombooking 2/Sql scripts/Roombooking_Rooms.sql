78a61ac045ea77619c79c17df1a736fd4cde7fd3
Bjørnar W. Risdalen <bwendelborgrisdalen@gmail.com>
2019-09-24 12:13:03 +0200

    Updated the Roombooking creation script. Otherwise minor changes.

--- Roombooking 2/Sql scripts/Roombooking_Rooms.sql
+++ Roombooking 2/Sql scripts/Roombooking_Rooms.sql
@@ -1,31 +1,36 @@
-CREATE DATABASE IF NOT EXISTS Roombooking;
-USE Roombooking;
-
-CREATE TABLE IF NOT EXISTS Rooms
+CREATE TABLE Roombooking.`Order`
 (
-  Room_ID int UNIQUE not null,
-  Room_building VARCHAR(255) NULL,
-  Room_maxCapacity int NULL,
-  CONSTRAINT R_roomID_PK PRIMARY KEY (Room_ID)
+    orderID varchar(255) PRIMARY KEY NOT NULL,
+    userID int(11),
+    roomID varchar(255),
+    CONSTRAINT O_userID_FK FOREIGN KEY (userID) REFERENCES User,
+    CONSTRAINT O_roomID_FK FOREIGN KEY (roomID) REFERENCES Rooms
 );
-
-INSERT INTO Roombooking.Rooms (roomID, roomFloor) VALUES ('E102', 'Floor 1');
-INSERT INTO Roombooking.Rooms (roomID, roomFloor) VALUES ('E202', 'Floor 2');
-INSERT INTO Roombooking.Rooms (roomID, roomFloor) VALUES ('E303', 'Floor 3');
-INSERT INTO Roombooking.Rooms (roomID, roomFloor) VALUES ('E4046', 'E4');
-INSERT INTO Roombooking.Rooms (roomID, roomFloor) VALUES ('E5045', 'E5');
-INSERT INTO Roombooking.Rooms (roomID, roomFloor) VALUES ('E808', 'Rom 80');
-INSERT INTO Roombooking.Rooms (roomID, roomFloor) VALUES ('E320', 'Rom 50');
-INSERT INTO Roombooking.Rooms (roomID, roomFloor) VALUES ('E3203', 'Østsia');
-
-CREATE TABLE IF NOT EXISTS User
+CREATE INDEX O_userID_FK ON Roombooking.`Order` (userID);
+CREATE INDEX O_roomID_FK ON Roombooking.`Order` (roomID);
+CREATE TABLE Roombooking.Rooms
 (
-  User_ID        INT AUTO_INCREMENT
-    PRIMARY KEY,
-  User_firstName VARCHAR(20)  NOT NULL,
-  User_lastName  VARCHAR(35)  NOT NULL,
-  User_email     VARCHAR(40)  NOT NULL,
-  User_dob       VARCHAR(40)  NOT NULL,
-  User_password  VARCHAR(255) NULL,
-  User_salt      VARCHAR(100) NULL
+    roomID varchar(255) PRIMARY KEY NOT NULL,
+    roomFloor varchar(255),
+    maxCapacity int(11)
 );
+CREATE UNIQUE INDEX roomID ON Roombooking.Rooms (roomID);
+INSERT INTO Roombooking.Rooms (roomID, roomFloor, maxCapacity) VALUES ('E102', 'Floor 1', 10);
+INSERT INTO Roombooking.Rooms (roomID, roomFloor, maxCapacity) VALUES ('E202', 'Floor 2', 10);
+INSERT INTO Roombooking.Rooms (roomID, roomFloor, maxCapacity) VALUES ('E303', 'Floor 3', 10);
+INSERT INTO Roombooking.Rooms (roomID, roomFloor, maxCapacity) VALUES ('E320', 'Rom 50', 10);
+INSERT INTO Roombooking.Rooms (roomID, roomFloor, maxCapacity) VALUES ('E3203', 'Østsia', 10);
+INSERT INTO Roombooking.Rooms (roomID, roomFloor, maxCapacity) VALUES ('E4046', 'E4', 10);
+INSERT INTO Roombooking.Rooms (roomID, roomFloor, maxCapacity) VALUES ('E5045', 'E5', 10);
+INSERT INTO Roombooking.Rooms (roomID, roomFloor, maxCapacity) VALUES ('E808', 'Rom 80', 10);
+CREATE TABLE Roombooking.User
+(
+    User_ID int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    User_firstName varchar(20) NOT NULL,
+    User_lastName varchar(35) NOT NULL,
+    User_email varchar(40) NOT NULL,
+    User_dob varchar(40) NOT NULL,
+    User_password varchar(255),
+    User_salt varchar(100)
+);
+CREATE UNIQUE INDEX User_email ON Roombooking.User (User_email);
