cb4584cc1feda29f8309d4d7a7c57e6edf01305e
Nosp <trymstaur@hotmail.com>
2019-09-26 16:44:56 +0200

    Started working on Dbfunc retreive adminemail

--- Roombooking 2/src/Classes/Email/EmailUtil.java
+++ Roombooking 2/src/Classes/Email/EmailUtil.java
@@ -1,21 +1,12 @@
 package Classes.Email;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
 
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileDataSource;
-import javax.mail.BodyPart;
+import java.util.Date;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 
 public class EmailUtil {
 
@@ -34,17 +25,11 @@ public class EmailUtil {
             msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
             msg.addHeader("format", "flowed");
             msg.addHeader("Content-Transfer-Encoding", "8bit");
-
             msg.setFrom(new InternetAddress("grproom@gmail.com", "NoReply-RoomBooking"));
-
             msg.setReplyTo(InternetAddress.parse("grproom@gmail.com", false));
-
             msg.setSubject(subject, "UTF-8");
-
             msg.setText(body, "UTF-8");
-
             msg.setSentDate(new Date());
-
             msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
             System.out.println("Message is ready");
             Transport.send(msg);
