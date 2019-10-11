d1d68471809348fccd598bdaae13e928bfaefc6c
Alena <alenaporfoevakorr@gmail.com>
2019-10-11 12:20:28 +0200

    fixed structure

---
+++ Roombooking 2/src/Classes/UserType.java
@@ -0,0 +1,8 @@
+
+package Classes;
+
+public enum UserType {
+    STUDENT,
+    TEACHER,
+    ADMIN;
+}
---
+++ Roombooking 2/src/Passwords/PasswordHashAndCheck.java
@@ -0,0 +1,77 @@
+package Passwords;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
+public class PasswordHashAndCheck {
+
+    public static int iterations = 1000;
+
+    public PasswordHashAndCheck() { }
+
+    public String stringToSaltedHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return privateStringToSaltedHash(password);
+    }
+
+    private String privateStringToSaltedHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        char[] chars = password.toCharArray();
+        byte[] salt = getSalt();
+
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = skf.generateSecret(spec).getEncoded();
+        return toHex(salt) + ":" + toHex(hash);
+    }
+
+    public boolean validatePassword(String originalPassword, String storedPassword) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return privateValidatePassword(originalPassword, storedPassword);
+    }
+
+    private boolean privateValidatePassword(String originalPassword, String storedPassword) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String[] parts = storedPassword.split(":");
+        byte[] salt = fromHex(parts[0]);
+        byte[] hash = fromHex(parts[1]);
+
+        PBEKeySpec spec = new PBEKeySpec(originalPassword.toCharArray(), salt, iterations, hash.length * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] testHash = skf.generateSecret(spec).getEncoded();
+
+        String pass1 = toHex(hash);
+        String pass2 = toHex(testHash);
+        return pass1.equals(pass2);
+    }
+
+    private byte[] getSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
+    }
+
+    private static String toHex(byte[] array) throws NoSuchAlgorithmException {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+        int paddingLength = (array.length * 2) - hex.length();
+        if(paddingLength > 0)
+        {
+            return String.format("%0"  +paddingLength + "d", 0) + hex;
+        }else{
+            return hex;
+        }
+    }
+
+    private static byte[] fromHex(String hex) throws NoSuchAlgorithmException
+    {
+        byte[] bytes = new byte[hex.length() / 2];
+        for(int i = 0; i<bytes.length ;i++)
+        {
+            bytes[i] = (byte)Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        }
+        return bytes;
+    }
+
+}
