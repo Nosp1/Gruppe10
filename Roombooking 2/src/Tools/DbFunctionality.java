9819eafe82429335712d825c90e63840607c0ee7
Nosp <trymstaur@hotmail.com>
2019-10-14 13:08:17 +0200

    Temp begining update orders

--- Roombooking 2/src/Tools/DbFunctionality.java
+++ Roombooking 2/src/Tools/DbFunctionality.java
@@ -348,14 +348,13 @@ public class DbFunctionality {
         System.out.println("update order started");
 
         String updateOrder = "UPDATE `order` "
-                + "set Timestamp_start ?"
+                + "set Timestamp_start = ?"
+               // + "set Timestamp_end = ?"
                 + "WHERE order_id = ?";
 
         updateOrderPS = connection.prepareStatement(updateOrder);
         updateOrderPS.setTimestamp(1, order.getTimestampStart());
-        updateOrderPS.setTimestamp(1, order.getTimestampEnd());
-
-
+        //updateOrderPS.setTimestamp(1, order.getTimestampEnd());
         updateOrderPS.execute();
 
     }
