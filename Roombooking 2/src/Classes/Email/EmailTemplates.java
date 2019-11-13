f7ec6082c00d24124fc74ceff347ed2b9a1e230e
Nosp <trymstaur@hotmail.com>
2019-11-13 11:26:59 +0100

    fixed email template to now take group room name

--- Roombooking 2/src/Classes/Email/EmailTemplates.java
+++ Roombooking 2/src/Classes/Email/EmailTemplates.java
@@ -31,7 +31,7 @@ public class EmailTemplates {
      */
     public static String bookingConfirmation(String recipientEmailName, Order order) {
         return "Reservation number " + order.getID() + "\n" +
-                recipientEmailName + " you have successfully booked " + order.getRoomID() +
+                recipientEmailName + " you have successfully booked " + order.getRoomName() +
                 " from " + order.getTimestampStart() + " -- " + order.getTimestampEnd()
                 + "\n" + "for any inquires regarding your order please send us an email at \n" +
                 "grproom@gmail.com with your order reservation number found in this email" + "\n" +
